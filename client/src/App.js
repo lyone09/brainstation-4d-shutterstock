fab21ead271a0e835dc822344a90295b7ea9765e
Ethan Lyon <73044606+lyone09@users.noreply.github.com>
2021-01-22 14:01:11 -0500

    added trending component and page

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,12 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Redirect } from 'react-router-dom';
 import "./App.scss";
-import { Trending } from './pages';
+import Trending from './pages/Trending/Trending';
 import { Header } from './components';
 
 function App() {
   return (
     <Router>
-      <h1>Brainstation 4D ShutterStock</h1>
-      <Header/>
       <Switch>
         <Route exact path='/' render={(props) => 
           <Trending {...props} />} 
